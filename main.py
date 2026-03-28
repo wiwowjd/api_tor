5a94c9b0de2f2c67c3e54cd67c3eadcbf2661d3d
wiwowjd <xinzuofficial098@gmail.com>
2026-03-28 20:31:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from torrequest import TorRequest
 
@@ -9,26 +10,13 @@ def home():
 
 @app.route("/ip")
 def get_ip():
-    try:
-        with TorRequest(proxy_port=9050, ctrl_port=9051) as tr:
-            res = tr.get("http://httpbin.org/ip")
-            return jsonify({"ip": res.json()})
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-@app.route("/new-ip")
-def new_ip():
-    try:
-        with TorRequest(proxy_port=9050, ctrl_port=9051) as tr:
-            tr.reset_identity()
-            res = tr.get("http://httpbin.org/ip")
-            return jsonify({"new_ip": res.json()})
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    with TorRequest(proxy_port=9050, ctrl_port=9051) as tr:
+        return tr.get("http://httpbin.org/ip").text
 
 @app.errorhandler(404)
 def not_found(e):
     return jsonify({"error": "endpoint not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # 🔥 penting
+    app.run(host="0.0.0.0", port=port)
